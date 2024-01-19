dcf97b8b60dc65cc2da692e0a53e8be4b7d25e9f
Sascha Holzigel <S.Holzigel@gmx.de>
2024-01-19 14:11:51 +0100

    Klasse-Fluglizenz initialisiert + getter und setter für alle eigenschaften erstellt.

--- src/Fluglizenz.java
+++ src/Fluglizenz.java
@@ -1,4 +1,40 @@
+import java.util.HashMap;
+
 public class Fluglizenz {
 
+    public static HashMap<Integer, Fluglizenz> fluglizenzen = new HashMap<>();
+
+    private int id;
+    private Pilot pilot;
+    private Flugzeugtyp flugzeugtyp;
+
+    public Fluglizenz(int id, Pilot pilot, Flugzeugtyp flugzeugtyp){
+        this. id = id;
+        this.pilot = pilot;
+        this.flugzeugtyp = flugzeugtyp;
+
+        fluglizenzen.put(id, this);
+    }
+
+    // Getter für id
+    public  int getId(){
+        return this.id;
+    }
+
+    // Getter und Setter für Pilot
+    public Pilot getPilot() {
+        return this.pilot;
+    }
+    public void setPilot(){
+        this.pilot = pilot;
+    }
+
+    // Getter und Setter für flugzeugtyp
+    public Flugzeugtyp getFlugzeugtyp() {
+        return this.flugzeugtyp;
+    }
+    public void setFlugzeugtyp(Flugzeugtyp flugzeugtyp) {
+        this.flugzeugtyp = flugzeugtyp;
+    }
 
 }
