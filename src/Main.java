122569c2f5a7b181afe257ad777ca2993c79b8b7
Sascha Holzigel <S.Holzigel@gmx.de>
2024-01-19 15:04:32 +0100

    Java erfolgreich mit Datenbank verbunden.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        try{
+            MySQL.getConnection();
+        }catch(SQLException ex){
+            ex.printStackTrace();
+        }
     }
 }
