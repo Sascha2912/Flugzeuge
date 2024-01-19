39a5d1457a18dfca7adfb985ad6f05bd32eddf0d
Sascha Holzigel <S.Holzigel@gmx.de>
2024-01-19 14:40:04 +0100

    Klasse-Piloten initialisiert + getter und setter für alle eigenschaften erstellt.

--- src/Pilot.java
+++ src/Pilot.java
@@ -1,4 +1,44 @@
+import java.util.HashMap;
+
 public class Pilot {
 
+    public static HashMap<Integer, Pilot> piloten = new HashMap<>();
 
+    private final int id;
+    private String vorname;
+    private String nachname;
+
+    public Pilot(int id, String vorname, String nachname){
+        this.id = id;
+        this.vorname = vorname;
+        this.nachname = nachname;
+
+        piloten.put(id, this);
+    }
+
+    // Getter für piloten
+    public static HashMap<Integer, Pilot> getPiloten() {
+        return piloten;
+    }
+
+    // Getter für id
+    public int getId() {
+        return id;
+    }
+
+    // Getter und Setter für vorname
+    public String getVorname() {
+        return vorname;
+    }
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
+    }
+
+    // Getter nd Setter für nachname
+    public String getNachname() {
+        return nachname;
+    }
+    public void setNachname(String nachname) {
+        this.nachname = nachname;
+    }
 }
