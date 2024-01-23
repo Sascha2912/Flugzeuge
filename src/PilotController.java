ea4aa3fbfcbfbe220df6eb67d7164ceba6b79b0d
Sascha Holzigel <S.Holzigel@gmx.de>
2024-01-23 13:00:42 +0100

    Erfolgreich FlugzeugtypController selectFlugzeugtypen-Methode geschrieben und getestet, um die Flugzeugtypen in der Datenbank auf Objektebene zu spiegeln.

--- src/PilotController.java
+++ src/PilotController.java
@@ -1,4 +1,39 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class PilotController {
 
+    public static void selectPiloten(){
+        Connection connection = null;
+
+        try{
+            connection = MySQL.getConnection();
+            Statement statement = connection.createStatement();
+
+            ResultSet rs = statement.executeQuery("select * from pilot");
+
+            while (rs.next()) {
+                int id = rs.getInt(1);
+                String vorname = rs.getString(2);
+                String nachname = rs.getString(3);
+
+
+                new Pilot(id, vorname,nachname);
+
+            }
+        }catch(SQLException ex){
+            ex.printStackTrace();
+        }finally {
+            if(connection != null){
+                try{
+                    connection.close();
+                }catch(SQLException ex){
+                    ex.printStackTrace();
+                }
+            }
+        }
+    }
 
 }
