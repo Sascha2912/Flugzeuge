8117fa72ba791ec51c7f7e5a3444789784bb2fed
Sascha Holzigel <S.Holzigel@gmx.de>
2024-01-23 12:50:53 +0100

    Erfolgreich FlughafenFlugzeugController selectFlughafenFlugzeug-Methode geschrieben und getestet, um die Flughäfen mit erlaubten Flugzeugen in der Datenbank auf Objektebene zu spiegeln.

--- src/FlughafenFlugzeugController.java
+++ src/FlughafenFlugzeugController.java
@@ -1,4 +1,42 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class FlughafenFlugzeugController {
 
+    public static void selectFlughafenFlugzeug(){
+        Connection connection = null;
+
+        try{
+            connection = MySQL.getConnection();
+            Statement statement = connection.createStatement();
+
+            ResultSet rs = statement.executeQuery("select * from flughafen_flugzeug");
+
+            while (rs.next()) {
+                int id = rs.getInt(1);
+                int flughafenId = rs.getInt(2);
+                int flugzeugtypId = rs.getInt(3);
+
+                Flughafen flughafen = Flughafen.flughaefen.get(flughafenId);
+                Flugzeugtyp flugzeugtyp = Flugzeugtyp.flugzeugtypen.get(flugzeugtypId);
+
+
+                new FlughafenFlugzeug(id, flughafen, flugzeugtyp);
+
+            }
+        }catch(SQLException ex){
+            ex.printStackTrace();
+        }finally {
+            if(connection != null){
+                try{
+                    connection.close();
+                }catch(SQLException ex){
+                    ex.printStackTrace();
+                }
+            }
+        }
+    }
 
 }
